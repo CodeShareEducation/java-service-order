9fd29d8c098f02373a1fd18eefb7e0e5a07776a4
mcqueide <john.clemente@mtur.gov.br>
2016-12-12 12:54:47 -0200

    Removing service-order/service-order-angular as a modular server. Adding a new module to front-end, service-order-angular Updating README, putting new features to it. Updating how-to-config-data-source-wildfly.md, adding how to config the new project.

---
+++ service-order-angular/app/scripts/services.js
@@ -0,0 +1,12 @@
+/**
+ * Created by mcqueide on 10/12/16.
+ */
+'use strict';
+
+angular.module('serviceOrder')
+    .constant("baseURL", "http://localhost:8080/service-order-rest/rest/")
+    .service('clientService',["$resource","baseURL",function ($resource,baseURL){
+        this.getClients = function () {
+            return $resource(baseURL+"client/:id",null, {'update':{method:'PUT'}});
+        }
+    }]);
